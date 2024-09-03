ce0cac2f45738dc522f0373d13f7ded501b4c5fd
schaetti <snow2458-->
2024-09-03 15:32:06 +0200

    03/09/2024
    - Integration tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -57,8 +57,11 @@ class TestComplexEquationIntegration(unittest.TestCase):
         self.hook_counts = {
             'ts1': 0,
             'ts2': 0,
+            'ts3': 0,
             'tp1': 0,
+            'tp2': 0,
             'tm1': 0,
+            'tm2': 0,
             'matrix_product': 0,
             'mv_result': 0,
             'eq_part11': 0,
@@ -70,8 +73,11 @@ class TestComplexEquationIntegration(unittest.TestCase):
         # Register hooks
         self.ts1.add_event_listener("on_change", self.make_hook('ts1'))
         self.ts2.add_event_listener("on_change", self.make_hook('ts2'))
+        self.ts3.add_event_listener("on_change", self.make_hook('ts3'))
         self.tp1.add_event_listener("on_change", self.make_hook('tp1'))
+        self.tp2.add_event_listener("on_change", self.make_hook('tp2'))
         self.tm1.add_event_listener("on_change", self.make_hook('tm1'))
+        self.tm2.add_event_listener("on_change", self.make_hook('tm2'))
     # end setUp
 
     def make_hook(self, key):
@@ -114,7 +120,6 @@ class TestComplexEquationIntegration(unittest.TestCase):
         # [[4, 4], [10, 8]] x [1, 2] = [12, 26]
         # [-10, -10] + [12, 26] - 4 = [2, 16] - 4 = [-2, 12]
         # Initial checks
-        print("test1")
         self.assertAlmostEqual(equation.x, -2)  # Expected x value
         self.assertAlmostEqual(equation.y, 12)  # Expected y value
 
@@ -124,30 +129,47 @@ class TestComplexEquationIntegration(unittest.TestCase):
         # [[4, 4], [10, 8]] x [1, 2] = [12, 26]
         # [-16, -16] + [12, 26] - 4 = [-4, 10] - 4 = [-8, 6]
         # Modify the Scalars and check results
-        print("test2")
         self.s1.value = 5
+        # ts1 is modified (1)
+        # eq_part11 is modified (1)
+        # eq_part1 is modified (1)
+        # equation is modified (1)
         self.assertAlmostEqual(equation.x, -8)  # Expected x value after modification
         self.assertAlmostEqual(equation.y, 6)  # Expected y value after modification
+        self.assertEqual(self.hook_counts['ts1'], 1)  # s1 changed once
+        self.assertEqual(self.hook_counts['eq_part11'], 1)  # eq_part11 should update twice
+        self.assertEqual(self.hook_counts['eq_part1'], 1)  # eq_part1 should update twice
+        self.assertEqual(self.hook_counts['equation'], 1)  # Final equation should update seven times
 
         # (5 + 1) * ([1, 2] - [3, 4]) + ([[1, 2], [3, 4]] @ [[2, 0], [1, 2]]) x [1, 2] - 4
         # 6 * ([-2, -2]) + ([[1, 2], [3, 4]] @ [[2, 0], [1, 2]]) x [1, 2] - 4
         # ([[1, 2], [3, 4]] @ [[2, 0], [1, 2]]) = [[4, 4], [10, 8]]
         # [[4, 4], [10, 8]] x [1, 2] = [12, 26]
         # [-12, -12] + [12, 26] - 4 = [0, 14] - 4 = [-4, 10]
-        print("test3")
         self.s2.value = 1
+        # ts2 is modified
+        # eq_part11 is modified
+        # eq_part1 is modified
+        # equation is modified
         self.assertAlmostEqual(equation.x, -4)  # Expected x value after modification
         self.assertAlmostEqual(equation.y, 10)  # Expected y value after modification
+        self.assertEqual(self.hook_counts['ts2'], 1)  # s1 changed once
+        self.assertEqual(self.hook_counts['eq_part11'], 2)  # eq_part11 should update twice
+        self.assertEqual(self.hook_counts['eq_part1'], 2)  # eq_part1 should update twice
+        self.assertEqual(self.hook_counts['equation'], 2)  # Final equation should update seven times
 
         # (5 + 1) * ([1, 2] - [3, 4]) + ([[1, 2], [3, 4]] @ [[2, 0], [1, 2]]) x [1, 2] - 0
         # 6 * ([-2, -2]) + ([[1, 2], [3, 4]] @ [[2, 0], [1, 2]]) x [1, 2] - 0
         # ([[1, 2], [3, 4]] @ [[2, 0], [1, 2]]) = [[4, 4], [10, 8]]
         # [[4, 4], [10, 8]] x [1, 2] = [12, 26]
         # [-12, -12] + [12, 26] - 0 = [0, 14] - 0 = [0, 14]
-        print("test4")
         self.s3.value = 0
+        # ts3 is modified
+        # equation is modified
         self.assertAlmostEqual(equation.x, 0)  # Expected x value after modification
         self.assertAlmostEqual(equation.y, 14) # Expected y value after modification
+        self.assertEqual(self.hook_counts['ts3'], 1)  # s1 changed once
+        self.assertEqual(self.hook_counts['equation'], 3)  # Final equation should update seven times
 
         # Modify the Points and check results
         # (5 + 1) * ([4, 2] - [3, 4]) + ([[1, 2], [3, 4]] @ [[2, 0], [1, 2]]) x [4, 2] - 0
@@ -155,18 +177,30 @@ class TestComplexEquationIntegration(unittest.TestCase):
         # ([[1, 2], [3, 4]] @ [[2, 0], [1, 2]]) = [[4, 4], [10, 8]]
         # [[4, 4], [10, 8]] x [4, 2] = [24, 56]
         # [6, -12] + [24, 56] - 0 = [30, 44] - 0 = [30, 44]
-        print("test5")
         self.p1.x = 4
+        # tp1 is modified
+        # mv_result is modified
+        # equation is modified
         self.assertAlmostEqual(equation.x, 30)  # Expected x value after modification
+        self.assertEqual(self.hook_counts['tp1'], 1)  # s1 changed once
+        self.assertEqual(self.hook_counts['mv_result'], 1)
+        self.assertEqual(self.hook_counts['equation'], 4)  # Final equation should update seven times
 
         # (5 + 1) * ([4, 2] - [3, 1]) + ([[1, 2], [3, 4]] @ [[2, 0], [1, 2]]) x [4, 2] - 0
         # 6 * ([1, 1]) + ([[1, 2], [3, 4]] @ [[2, 0], [1, 2]]) x [4, 2] - 0
         # ([[1, 2], [3, 4]] @ [[2, 0], [1, 2]]) = [[4, 4], [10, 8]]
         # [[4, 4], [10, 8]] x [4, 2] = [24, 56]
         # [6, 6] + [24, 56] - 0 = [30, 62] - 0 = [30, 62]
-        print("test6")
         self.p2.y = 1
+        # tp2 is modified
+        # eq_part12 is modified
+        # eq_part1 is modified
+        # equation is modified
         self.assertAlmostEqual(equation.y, 62)  # Expected y value after modification
+        self.assertEqual(self.hook_counts['tp2'], 1)  # s1 changed once
+        self.assertEqual(self.hook_counts['eq_part12'], 1)
+        self.assertEqual(self.hook_counts['eq_part1'], 3)
+        self.assertEqual(self.hook_counts['equation'], 4)  # Final equation should update seven times
 
         # Modify the Matrices and check results
         # (5 + 1) * ([4, 2] - [3, 1]) + ([[2, 1], [1, 3]] @ [[2, 0], [1, 2]]) x [4, 2] - 0
@@ -174,31 +208,40 @@ class TestComplexEquationIntegration(unittest.TestCase):
         # ([[2, 1], [1, 3]] @ [[2, 0], [1, 2]]) = [[5, 2], [5, 6]]
         # [[5, 2], [5, 6]] x [4, 2] = [24, 36]
         # [6, 6] + [24, 36] - 0 = [30, 42] - 0 = [30, 42]
-        print("test7")
         self.m1.data = np.array([[2, 1], [1, 3]])
+        # tm1 is modified
+        # matrix_product is modified
+        # mv_result is modified
+        # equation is modified
         self.assertAlmostEqual(equation.x, 30)  # Expected x value after modification
+        self.assertEqual(self.hook_counts['tm1'], 1)  # s1 changed once
+        self.assertEqual(self.hook_counts['matrix_product'], 1)
+        self.assertEqual(self.hook_counts['mv_result'], 2)
+        self.assertEqual(self.hook_counts['equation'], 5)  # Final equation should update seven times
 
         # (5 + 1) * ([4, 2] - [3, 1]) + ([[2, 1], [1, 3]] @ [[1, 0], [0, 1]]) x [4, 2] - 0
         # 6 * ([1, 1]) + ([[2, 1], [1, 3]] @ [[1, 0], [0, 1]]) x [4, 2] - 0
         # ([[2, 1], [1, 3]] @ [[1, 0], [0, 1]]) = [[2, 1], [1, 3]]
         # [[2, 1], [1, 3]] x [4, 2] = [10, 10]
         # [6, 6] + [10, 10] - 0 = [16, 16] - 0 = [16, 16]
-        print("test8")
         self.m2.data = np.array([[1, 0], [0, 1]])
+        # tm2 is modified
+        # matrix_product is modified
+        # mv_result is modified
+        # equation is modified
         self.assertAlmostEqual(equation.y, 16)  # Expected y value after modification
-        print(self.hook_counts)
-        # Check if hooks were called the expected number of times
-        print("test9")
         self.assertEqual(self.hook_counts['ts1'], 1)  # s1 changed once
         self.assertEqual(self.hook_counts['ts2'], 1)  # s2 changed once
+        self.assertEqual(self.hook_counts['ts3'], 1)  # ts3 changed once
         self.assertEqual(self.hook_counts['tp1'], 1)  # p1 changed once
         self.assertEqual(self.hook_counts['tm1'], 1)  # m1 changed once
+        self.assertEqual(self.hook_counts['tm2'], 1)  # m3 changed once
         self.assertEqual(self.hook_counts['matrix_product'], 2)  # Matrix product should update twice
         self.assertEqual(self.hook_counts['mv_result'], 3)  # mv_result should update three times
         self.assertEqual(self.hook_counts['eq_part11'], 2)  # eq_part11 should update twice
-        self.assertEqual(self.hook_counts['eq_part12'], 2)  # eq_part12 should update twice
-        self.assertEqual(self.hook_counts['eq_part1'], 2)  # eq_part1 should update twice
-        self.assertEqual(self.hook_counts['equation'], 7)  # Final equation should update seven times
+        self.assertEqual(self.hook_counts['eq_part12'], 1)  # eq_part12 should update twice
+        self.assertEqual(self.hook_counts['eq_part1'], 3)  # eq_part1 should update twice
+        self.assertEqual(self.hook_counts['equation'], 6)  # Final equation should update seven times
     # end test_complex_equation
 
 # end class TestComplexEquationIntegration
